791940fd7b117dc386d42d99a9da77054b5eaefa
Archana <94049791+archanacodes@users.noreply.github.com>
2024-07-16 11:07:19 +0530

    Add files via upload

---
+++ achu.js
@@ -0,0 +1,54 @@
+// var n=document.getElementById("r").value
+// var t=document.getElementById("e").value
+// var g=prompt("enter")
+// switch(g){
+//     case '+':{
+//        var h=parseInt(n)+parseInt(t)
+//        document.getElementById("j").h
+//        break
+//     }
+//     case '-':{
+//         var h=parseInt(n)-parseInt(t)
+//         document.getElementById("j").h
+//         break
+//     }
+//     case '*':{
+//         var h=parseInt(n)*parseInt(t)
+//        document.getElementById("j").h
+//        break
+//     }
+//     case '/':{
+//         var h=parseInt(n)/parseInt(t)
+//        document.getElementById("j").innerHTML=h
+//        break
+//     }
+// }
+
+function achu(){
+    var n=document.getElementById("r").value
+var t=document.getElementById("e").value
+    var h=parseInt(n)+parseInt(t)
+    document.getElementById("j").innerHTML=h
+    
+}
+function anna(){
+    var n=document.getElementById("r").value
+var t=document.getElementById("e").value
+    var h=parseInt(n)-parseInt(t)
+    document.getElementById("j").innerHTML=h
+    
+}
+function appa(){
+    var n=document.getElementById("r").value
+var t=document.getElementById("e").value
+    var h=parseInt(n)*parseInt(t)
+    document.getElementById("j").innerHTML=h
+    
+}
+function amma(){
+    var n=document.getElementById("r").value
+var t=document.getElementById("e").value
+    var h=parseInt(n)/parseInt(t)
+    document.getElementById("j").innerHTML=h
+    
+}
